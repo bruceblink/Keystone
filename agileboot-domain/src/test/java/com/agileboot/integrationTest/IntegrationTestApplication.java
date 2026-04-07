3cb7f145072845cdfb243175b1fe725dc2869835
liukang <likanug.g@qq.com>
2026-04-07 17:56:43 +0800

    fix(test): fix IntegrationTestApplication Spring context config
    - Replace invalid glob @ComponentScan('com.agileboot.*') with
      scanBasePackages attribute on @SpringBootApplication, which correctly
      scans all sub-packages under com.agileboot
    - Add @MapperScan(basePackages='com.agileboot', annotationClass=Mapper.class)
      to register all @Mapper-annotated interfaces in the test application context
    - Enable H2 console and add mybatis-plus config in application.yml for tests

--- agileboot-domain/src/test/java/com/agileboot/integrationTest/IntegrationTestApplication.java
+++ agileboot-domain/src/test/java/com/agileboot/integrationTest/IntegrationTestApplication.java
@@ -1,5 +1,7 @@
 package com.agileboot.integrationTest;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,8 +11,8 @@ import org.springframework.context.annotation.ComponentScan;
  * 集成测试配置类
  * @author valarchie
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = "com.agileboot.*")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}, scanBasePackages = "com.agileboot")
+@MapperScan(basePackages = "com.agileboot", annotationClass = Mapper.class)
 public class IntegrationTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(IntegrationTestApplication.class, args);
