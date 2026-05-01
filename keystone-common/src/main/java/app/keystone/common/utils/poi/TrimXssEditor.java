a42f70047d405d904a8605d34dee64fe27bc0233
bruceblink <likanug.g@qq.com>
2026-05-01 20:31:13 +0800

    refactor(common): replace hutool html cleaning in excel xss editor
    Use regex-based HTML tag stripping in TrimXssEditor to remove hutool HtmlUtil dependency while preserving XSS sanitization behavior.

--- keystone-common/src/main/java/app/keystone/common/utils/poi/TrimXssEditor.java
+++ keystone-common/src/main/java/app/keystone/common/utils/poi/TrimXssEditor.java
@@ -1,6 +1,5 @@
 package app.keystone.common.utils.poi;
 
-import cn.hutool.http.HtmlUtil;
 import cn.hutool.poi.excel.cell.CellEditor;
 import org.apache.poi.ss.usermodel.Cell;
 
@@ -13,7 +12,7 @@ public class TrimXssEditor implements CellEditor {
     @Override
     public Object edit(Cell cell, Object value) {
         if (value instanceof String) {
-            return HtmlUtil.cleanHtmlTag(value.toString());
+            return value.toString().replaceAll("<[^>]*>", "");
         }
         return value;
     }
